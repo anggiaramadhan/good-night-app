7276a912c518425c683a55c7b5bcae284674f2c8
Anggia Ramadhan <anggia.ramadhan@gmail.com>
2023-04-28 12:42:30 +0700

    add user validation and test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should allow create new user with valid name' do
+    user = User.new(name: 'Jane Doe')
+    assert user.save, 'success create new user with valid name'
+  end
+
+  test 'should not allow create new user with empty name' do
+    user = User.new(name: '')
+    assert_not user.save, 'failed to create new user with empty name'
+  end
+
+  test 'should not allow create new user with name more than 50 chars' do
+    user = User.new(name: 'A sessional instructor of Interaction Design at Emily Carr University of Art and Design')
+    assert_not user.save, 'failed to create new user with more than 50 chars name'
+  end
 end
