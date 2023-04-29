8f7aeebc339e89e064d87a50820bcd92a677289a
Anggia Ramadhan <anggia.ramadhan@gmail.com>
2023-04-29 17:23:34 +0700

    add testing for session management

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: user_params['name'])
-    token = @user.encode_token
-    render json: { token: }, status: :ok
+    if @user.present?
+      token = @user.encode_token
+      render json: { token: }, status: :ok
+    else
+      render json: { message: 'user not found' }, status: :not_found
+    end
   end
 
   def destroy; end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,13 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get sessions_create_url
+  test 'should get create new session when name is valid' do
+    post sessions_url, params: { user: { name: 'MyString' } }
     assert_response :success
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+  test 'should not create new session when name is invalid' do
+    post sessions_url, params: { user: { name: 'YourString' } }
+    assert_response :not_found
   end
 end
