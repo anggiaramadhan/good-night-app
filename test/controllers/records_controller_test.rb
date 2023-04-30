e1f7a6b3bcd91e46d4d2b5e45c0889429d8a254f
Anggia Ramadhan <anggia.ramadhan@gmail.com>
2023-04-30 11:22:48 +0700

    add mocking jwt for unit testing purpose

--- test/controllers/records_controller_test.rb
+++ test/controllers/records_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class RecordsControllerTest < ActionDispatch::IntegrationTest
+  test 'should response success to create a record' do
+    post records_url, headers: { 'Authorization': build_jwt }
+    assert_response :success
+  end
 end
